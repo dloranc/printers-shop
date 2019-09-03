d8a43ca3a8803ad9e3dfe10cd0c5685cc45184a7
Dawid Loranc <d.loranc@selleo.com>
2019-09-03 16:01:45 +0200

    Show path name in the ScreensNoMatch component

--- src/screens/NoMatch/index.js
+++ src/screens/NoMatch/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
-function ScreensNoMatch() {
+function ScreensNoMatch({ location }) {
   return (
     <div className="error-404">
       <h1>Page not found</h1>
 
-      <p>Return to the <Link to="/">home page</Link>.</p>
+      <p>
+        The { location.pathname } page couldn't be found.
+        Return to the <Link to="/">home page</Link>.
+      </p>
     </div>
   );
 }
 
-export default ScreensNoMatch;
+export default withRouter(ScreensNoMatch);
