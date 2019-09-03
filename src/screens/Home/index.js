ad12acc6f8f1cab7dcc464467b821085405c7cbd
Dawid Loranc <d.loranc@selleo.com>
2019-09-03 13:30:57 +0200

    Add information to the Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Home() {
   return (
     <div className="home">
-      home page
+      <h1>Home</h1>
+
+      <p>Please, sign up or log in to see our products.</p>
     </div>
   );
 }
