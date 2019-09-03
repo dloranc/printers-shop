c84749ba1b14ffdec1dca41281f0878b8d643807
Dawid Loranc <d.loranc@selleo.com>
2019-09-03 13:37:17 +0200

    Move typical container code from screen component to the components/User/SignUp/Form/Container.js

---
+++ src/components/User/SignUp/Form/Container.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SignUpForm from './Form';
+
+class SignUpFormContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            companies: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:4000/companies').then(response => {
+            return response.json();
+        }).then(response => {
+            this.setState(
+                {
+                    companies: response
+                }
+            );
+        });
+    }
+
+    render() {
+        return <SignUpForm companies={this.state.companies}/>
+    }
+}
+
+export default SignUpFormContainer;
--- src/screens/SignUp/Form.js
+++ src/screens/SignUp/Form.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import SignUpForm from '../../components/User/SignUp/Form/Form';
+import SignUpFormContainer from '../../components/User/SignUp/Form/Container';
 
 class SignUpFormScreen extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            companies: [],
-        }
-    }
-
-    componentDidMount() {
-        fetch('http://localhost:4000/companies').then(response => {
-            return response.json();
-        }).then(response => {
-            this.setState(
-                {
-                    companies: response
-                }
-            );
-        });
-    }
-
     render() {
-        return <SignUpForm companies={this.state.companies}/>
+        return <SignUpFormContainer/>
     }
 }
 
