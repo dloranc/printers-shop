70548a70f5431d6e0c63b491d0da2a630eb46c60
Dawid Loranc <d.loranc@selleo.com>
2019-09-04 15:07:12 +0200

    Redirect from / to /shop if the user is signed in

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { store } from './../../store';
 
 function ScreensHome() {
-  return (
-    <div className="home">
-      <h1>Home</h1>
+  if (!store.getState().isAuthenticated) {
+    return (
+      <div className="home">
+        <h1>Home</h1>
 
-      <p>Please, sign up or log in to see our products.</p>
-    </div>
-  );
+        <p>Please, sign up or log in to see our products.</p>
+      </div>
+    );
+  }
+
+  return <Redirect to="/shop"></Redirect>;
 }
 
 export default ScreensHome;
