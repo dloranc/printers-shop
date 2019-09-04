f51b683bc2a2b04bb6c0dbff73ab6f16e7871cfe
Dawid Loranc <d.loranc@selleo.com>
2019-09-04 11:41:15 +0200

    Remove App.css and move its content to index.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
 import ScreensRoot from './screens/Root';
