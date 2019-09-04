63ef647edcc176bb515dc46df73a995eed303389
Dawid Loranc <d.loranc@selleo.com>
2019-09-04 13:47:28 +0200

    Create product cards

--- src/components/Product/List/Container/Container.js
+++ src/components/Product/List/Container/Container.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
 
 import Product from './../../Product/Product';
+
+const ProductList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1em;
+`;
 
 export class ProductListContainer extends Component {
     constructor(props) {
@@ -19,11 +26,11 @@ export class ProductListContainer extends Component {
 
     render() {
         return (
-            <div>
+            <ProductList>
                 {
                     this.state.products.map(product => <Product key={product.id} {...product}></Product>)
                 }
-            </div>
+            </ProductList>
         )
     }
 }
--- src/components/Product/Product/Product.js
+++ src/components/Product/Product/Product.js
@@ -1,13 +1,125 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const ProductCard = styled.div`
+    width: 100%;
+    padding: 1em;
+    background: #fefefe;
+    color: #333;
+    box-shadow: 0 0 0.4em #ccc;
+`;
+
+const Title = styled.h2`
+    font-weight: lighter;
+    margin-bottom: 0.5em;
+`;
+
+const ProductRow = styled.div`
+    color: #666;
+`;
+
+const ProductLabel = styled.span`
+    color: black;
+    font-weight: bold;
+`;
+
+const AmountLabel = styled(ProductLabel)`
+    margin-top: 2em;
+`;
+
+const AmountInput = styled.input`
+    margin: 0.3em 0;
+    padding: 0.3em 0.5em;
+    width: 100%;
+`;
+
+const AddToCartButton = styled.button`
+    display: block;
+    margin: 0 auto;
+    margin-top: 1em;
+    padding: 0.6em 1em;
+    color: white;
+    background: #007bff;
+
+    :hover {
+        background: #118cff;
+    }
+`;
 
 export class Product extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            amount: 0,
+        }
+
+        this.inStock = this.inStock.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    inStock() {
+        const inStock = this.props.inStock;
+
+        if (inStock > 100) {
+            return 'full supply';
+        }
+
+        if (inStock > 10) {
+            return 'medium supply';
+        }
+
+        if (inStock > 0) {
+            return 'last pieces';
+        }
+
+        return 'not available';
+    }
+
+    handleChange(event) {
+        const value = event.target.value;
+
+        if (value < 0) {
+            this.setState({ amount: 0 });
+        } else {
+            this.setState({ amount: value });
+        }
+    }
+
     render() {
         return (
-            <div>
-                <div>{this.props.type} {this.props.name}</div>
-                <div>${this.props.price}</div>
-                <div>Quantity: {this.props.inStock}</div>
-            </div>
+            <ProductCard>
+                <Title>{this.props.type} {this.props.name}</Title>
+
+                <ProductRow>
+                    <ProductLabel>Price per unit:</ProductLabel> ${this.props.price}
+                </ProductRow>
+
+                <ProductRow>
+                    <ProductLabel>In stock:</ProductLabel> {this.inStock()}
+                </ProductRow>
+
+                <AmountLabel
+                    as="label"
+                    htmlFor={'amount-' + this.props.id}
+                >
+                    Amount of items to order:
+                </AmountLabel>
+
+                <AmountInput
+                    type="number"
+                    name="amount"
+                    id={'amount-' + this.props.id}
+                    value={this.state.amount}
+                    onChange={this.handleChange}
+                />
+
+                <ProductRow>
+                    <ProductLabel>Price:</ProductLabel> ${this.state.amount * this.props.price}
+                </ProductRow>
+
+                <AddToCartButton>Add to cart</AddToCartButton>
+            </ProductCard>
         )
     }
 }
