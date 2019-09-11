67818e7ddaece92f9aa4540a8b872b5fe39977b5
Dawid Loranc <d.loranc@selleo.com>
2019-09-11 08:51:31 +0200

    Add animated transition between routes

--- src/screens/Root/Root.js
+++ src/screens/Root/Root.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { withRouter, Switch, Route } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import ScreensSignUpForm from '../SignUp/Form';
 import ScreensSignInForm from '../SignIn/Form';
@@ -15,22 +16,46 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import './routeTransition.css';
+
+const routes = [
+    { path: '/', name: 'Home', Component: ScreensHome, exact: true },
+    { path: '/sign-up', name: 'Sign up', Component: ScreensSignUpForm, exact: false },
+    { path: '/sign-in', name: 'Sign in', Component: ScreensSignInForm, exact: false },
+    { path: '/shop', name: 'Shop', Component: ScreensShop, exact: false },
+    { path: '/cart', name: 'Cart', Component: ScreensCart, exact: false },
+    { path: '/orders', name: 'Orders', Component: ScreensOrders, exact: false },
+    { path: '/inventory', name: 'Inventory', Component: ScreensInventory, exact: false },
+    { path: null, name: '404', Component: ScreensNoMatch, exact: false },
+]
+
 class ScreensRoot extends React.Component {
     render() {
         return (
             <Container>
                 <Row>
                     <Col>
-                        <Switch>
-                            <Route path="/" exact component={ScreensHome}/>
-                            <Route path="/sign-up" component={ScreensSignUpForm}/>
-                            <Route path="/sign-in" component={ScreensSignInForm}/>
-                            <Route path="/shop" component={ScreensShop}/>
-                            <Route path="/cart" component={ScreensCart}/>
-                            <Route path="/orders" component={ScreensOrders}/>
-                            <Route path="/inventory" component={ScreensInventory}/>
-                            <Route component={ScreensNoMatch}/>
-                        </Switch>
+                        <TransitionGroup>
+                            <CSSTransition
+                                key={this.props.location.key}
+                                timeout={500}
+                                classNames="page"
+                                mountOnEnter
+                                unmountOnExit
+                            >
+                                <Switch location={this.props.location}>
+                                    {routes.map(({ path, exact, Component }) => (
+                                        <Route key={path} exact={exact} path={path}>
+                                            {({ match }) => (
+                                                <div className="page">
+                                                    <Component />
+                                                </div>
+                                            )}
+                                        </Route>
+                                    ))}
+                                </Switch>
+                            </CSSTransition>
+                        </TransitionGroup>
                     </Col>
                 </Row>
             </Container>
@@ -38,4 +63,4 @@ class ScreensRoot extends React.Component {
     }
 }
 
-export default ScreensRoot;
+export default withRouter(ScreensRoot);
