93793891549e0aaa8a6747d4593f2354e265c610
Dawid Loranc <d.loranc@selleo.com>
2019-09-23 11:13:22 +0200

    Fix a state mutation in cart's reducer (#17)

--- src/store/cart/reducer.js
+++ src/store/cart/reducer.js
@@ -2,20 +2,23 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from './constants';
 
 function cart(state = [], action) {
   switch (action.type) {
-    case ADD_TO_CART:
+    case ADD_TO_CART: {
       const index = state.findIndex(
         product => product.id === action.product.id
       );
 
       if (index !== -1) {
-        const cart = state.slice();
+        const cart = [...state];
+        const product = { ...cart[index] };
 
-        cart[index].amount += action.product.amount;
+        product.amount += action.product.amount;
+        cart[index] = product;
 
         return cart;
       }
 
       return state.concat(action.product);
+    }
     case REMOVE_FROM_CART:
       return state.filter(product => product.id !== action.product.id);
     default:
