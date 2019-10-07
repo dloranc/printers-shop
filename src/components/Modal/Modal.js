142f2775fde77ca17db5996f63aa46df456d2e33
Dawid Loranc <d.loranc@selleo.com>
2019-10-07 16:50:13 +0200

    Add Modal component with Portal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import './Modal.sass';
+
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    // Create a div that we'll render the modal into. Because each
+    // Modal component has its own element, we can render multiple
+    // modal components into the modal container.
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    // Append the element into the DOM on mount. We'll render
+    // into the modal container element (see the HTML tab).
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    // Remove the element from the DOM when we unmount
+    modalRoot.removeChild(this.el);
+  }
+
+  render() {
+    // Use a portal to render the children into the element
+    return ReactDOM.createPortal(
+      // Any valid React child: JSX, strings, arrays, etc.
+      this.props.children,
+      // A DOM element
+      this.el,
+    );
+  }
+}
+
+export default Modal;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import  { Helmet } from 'react-helmet';
+import Modal from './../../components/Modal/Modal';
 
 class ScreensHome extends Component {
   static propTypes = {
     isAuthenticated: PropTypes.bool.isRequired
+  }
+
+  state = {
+    show: false
+  }
+
+  toggleModal = () => {
+    this.setState({ show: !this.state.show });
   }
 
   render() {
@@ -22,6 +31,29 @@ class ScreensHome extends Component {
 
             <p>Please, sign up or log in to see our products.</p>
           </div>
+
+          <button onClick={this.toggleModal}>
+            {this.state.show ? 'Hide' : 'Show'}
+          </button>
+
+          {this.state.show &&
+            <Modal>
+              <div className="modal">
+                <div className="modal__container">
+                  <div className="modal__header">Header</div>
+                  <div className="modal__body">Body</div>
+                  <div className="modal__footer">
+                    <button
+                      className="modal__button -close"
+                      onClick={this.toggleModal}
+                    >
+                    OK
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Modal>
+          }
         </>
       );
     }
