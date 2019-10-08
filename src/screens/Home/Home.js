2842de911604e30381aede09ab3bc2c0b4c7e4ef
Dawid Loranc <d.loranc@selleo.com>
2019-10-08 08:55:47 +0200

    Learning/portals (#22)
    * Add node-sass
    
    * Add Modal component with Portal
    
    * Create Modal with default structure and move previous code to ModalPortal
    
    * Remove need to have global #modal-root element

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import  { Helmet } from 'react-helmet';
+import Modal from './../../components/Modal/Modal';
 
 class ScreensHome extends Component {
   static propTypes = {
     isAuthenticated: PropTypes.bool.isRequired
+  }
+
+  state = {
+    show: false
+  }
+
+  toggleModal = () => {
+    this.setState({ show: !this.state.show });
   }
 
   render() {
@@ -22,6 +31,24 @@ class ScreensHome extends Component {
 
             <p>Please, sign up or log in to see our products.</p>
           </div>
+
+          <button onClick={this.toggleModal}>
+            {this.state.show ? 'Hide' : 'Show'}
+          </button>
+
+          {this.state.show &&
+            <Modal
+              header={'Header'}
+              body={'Body'}
+              footer={
+                <button onClick={this.toggleModal}>
+                  OK
+                </button>
+              }
+              onClose={this.toggleModal}
+            >
+            </Modal>
+          }
         </>
       );
     }
