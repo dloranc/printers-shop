a7538dc4418c0a43838c1e227fb4b9536cc944b4
Dawid Loranc <d.loranc@selleo.com>
2019-10-08 09:53:46 +0200

    Setup why-did-you-render

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import NavBarContainer from './components/NavBar/Container/Container';
 import ScreensRoot from './screens/Root/Root';
 
+if (process.env.NODE_ENV !== 'production') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js');
+  whyDidYouRender(React);
+}
+
 function App() {
   return (
     <Provider store={store}>
