b2b528c1584cb5a327d8d6f04819c66fc50b21e5
Dawid Loranc <d.loranc@selleo.com>
2019-10-09 08:44:35 +0200

    Style ErrorBoundary

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './ErrorBoundary.sass';
 
 class ErrorBoundary extends Component {
   static propTypes = {
@@ -18,12 +19,16 @@ class ErrorBoundary extends Component {
   render() {
     if (this.state.errorInfo) {
       return (
-        <div>
+        <div className="error-boundary">
           <h2>Something went wrong.</h2>
 
-          <p>{this.state.error && this.state.error.toString()}</p>
+          <p className="error-boundary__name">
+            {this.state.error && this.state.error.toString()}
+          </p>
 
-          <p>{this.state.errorInfo.componentStack}</p>
+          <p className="error-boundary__stack">
+            {this.state.errorInfo.componentStack}
+          </p>
         </div>
       );
     }
